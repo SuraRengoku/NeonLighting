3933af6ce7a37c4c1368faf9f66eb80cd44ddb30
SuraRengoku <jiahao.liu@rwth-aachen.de>
2024-10-06 03:29:03 +0800

    inclusion

---
+++ build/CMakeFiles/NeonLighting.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NeonLighting.dir/main.cpp.obj"
+  "CMakeFiles/NeonLighting.dir/main.cpp.obj.d"
+  "NeonLighting.exe"
+  "NeonLighting.exe.manifest"
+  "NeonLighting.pdb"
+  "libNeonLighting.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NeonLighting.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/NeonLighting.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for NeonLighting.
+# This may be replaced when dependencies are built.
