996ac42c318e2265cbc453e1b9c88078675f94b1
SuraRengoku <jiahao.liu@rwth-aachen.de>
2024-10-05 16:59:06 +0800

    windows

--- main.cpp
+++ main.cpp
@@ -11,16 +11,17 @@ double hit_sphere(const point3& center, double radius, const ray& r) {
 
     auto discriminant = b * b - 4 * a * c;
     //== 0  1 root, > 0  2 roots
-    if(discriminant < 0)
+    if (discriminant < 0) {
         return -1.0;
-    else
+    } else {
         return (-b - std::sqrt(discriminant)) / (2.0 * a);
+    }
 }
 
 color ray_color(const ray& r) {
-    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
-    if(t > 0.0) {
-        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+    auto t = hit_sphere(point3(0,0,-1), 0.5, r);
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
         return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
     }
     
