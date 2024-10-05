934971a87e1db556f042ff692290035fbfaeeeaa
jonas <jiahao.liu@rwth-aachen.de>
2024-10-05 15:45:22 +0800

    debug

--- main.cpp
+++ main.cpp
@@ -15,8 +15,8 @@ bool hit_sphere(const point3& center, double radius, const ray& r) {
 }
 
 color ray_color(const ray& r) {
-    if(hit_sphere(point3(0, 0, -1), 0.5, r))
-        return color(1, 0, 0);
+    // if(hit_sphere(point3(0, 0, -1), 0.5, r))
+    //     return color(1, 0, 0);
 
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1.0);
